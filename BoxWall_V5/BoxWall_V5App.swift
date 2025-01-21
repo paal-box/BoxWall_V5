f4de3409c5140a3e74fc7b7e36a5bccc933fe45f
paal-box <paal.selnes@gmail.com>
2025-01-21 13:00:55 +0100

    Initial Commit

---
+++ BoxWall_V5/BoxWall_V5App.swift
@@ -0,0 +1,17 @@
+//
+//  BoxWall_V5App.swift
+//  BoxWall_V5
+//
+//  Created by Paal Selnaes on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BoxWall_V5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxWall_V5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoxWall_V5
+//
+//  Created by Paal Selnaes on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
